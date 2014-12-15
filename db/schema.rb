64328488fefb8dc5d52a6ee804c301d4bb269ac7
tienlv <tienlv.hust@gmail.com>
2014-12-16 03:15:48 +0700

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215181957) do
+ActiveRecord::Schema.define(version: 20141215200811) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141215181957) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
